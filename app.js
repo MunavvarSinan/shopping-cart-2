6dfc073286af329b8e706a3a2794ce5742b83351
Nikhil Kilivayil <nikhilkilivayil@gmail.com>
2020-09-24 22:03:20 +0530

    Day 4 Completed,mongodb connection, product insertion,display products

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var adminRouter = require('./routes/admin');
 var hbs=require('express-handlebars')
 var app = express();
 var fileUpload=require('express-fileupload')
+var db=require('./config/connection')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -18,6 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+
+db.connect((err)=>{
+  if(err) console.log("Connection Error"+err);
+  else console.log("Database Connected to port 27017");
+  
+})
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
