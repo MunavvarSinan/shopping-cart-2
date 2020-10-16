f66dad8bd68fd37378975589602a1f71679ac79e
Nikhil Kilivayil <nikhilkilivayil@gmail.com>
2020-10-16 21:39:05 +0530

    Checkout form submit using ajax-pending

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -147,7 +147,7 @@ module.exports={
                     }
                     ).then((response)=>{
                     
-                        resolve(true)
+                        resolve({status:true})
                     })
                 }
         })
--- routes/user.js
+++ routes/user.js
@@ -64,8 +64,11 @@ router.get('/logout',(req,res)=>{
 })
 router.get('/cart',verifyLogin,async(req,res)=>{
   let products=await userHelpers.getCartProducts(req.session.user._id)
+  let totalValue=await userHelpers.getTotalAmount(req.session.user._id)
   console.log(products);
-  res.render('user/cart',{products,user:req.session.user})
+  let user=req.session.user._id
+  console.log(user);
+  res.render('user/cart',{products,user,totalValue})
 })
 
 router.get('/add-to-cart/:id',(req,res)=>{
@@ -76,13 +79,16 @@ router.get('/add-to-cart/:id',(req,res)=>{
 })
 router.post('/change-product-quantity',(req,res,next)=>{
   console.log(req.body);
-  userHelpers.changeProductQuantity(req.body).then((response)=>{
+
+  userHelpers.changeProductQuantity(req.body).then(async(response)=>{
+    response.total=await userHelpers.getTotalAmount(req.body.user)
+      
       res.json(response)
   })
 })
 router.get('/place-order',verifyLogin, async(req,res)=>{
   let total=await userHelpers.getTotalAmount(req.session.user._id)
-  res.render('user/place-order',{total})
+  res.render('user/place-order',{total,user:req.session.user})
 })
 
 module.exports = router;
